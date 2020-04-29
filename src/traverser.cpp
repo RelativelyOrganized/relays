8765c5338dbef63cf9641201580030ef72b8a13b
Emmanuel Durand <emmanueldurand@gmail.com>
2020-04-28 21:24:05 -0400

    Moved test code from relays.cpp to check_flip_flop

--- src/traverser.cpp
+++ src/traverser.cpp
@@ -25,10 +25,9 @@ void Traverser::update(Circuit& circuit)
     // Relays outputs update step
     Relay* relay = &circuit._relays.at(0);
     const std::size_t relays_count = circuit._relays.size();
-    for (std::size_t i = 0; i < relays_count; i++)
+    for (std::size_t i = 0; i < relays_count; ++i, ++relay)
     {
         relay->output = (relay->input && relay->command);
-        relay++;
     }
 }
 
@@ -40,13 +39,12 @@ void Traverser::transfer(Circuit& circuit)
 
     Connection* connection = &circuit._connections.at(0);
     const std::size_t connections_count = circuit._connections.size();
-    for (std::size_t i = 0; i < connections_count; i++)
+    for (std::size_t i = 0; i < connections_count; ++i, ++connection)
     {
         Relay* from = &circuit._relays.at(connection->from);
         Relay* to = &circuit._relays.at(connection->to);
         bool* to_field = (&to->input + static_cast<int>(connection->slot));
         *to_field = from->output != connection->invert;
-        connection++;
     }
 }
 
---
+++ tests/check_flip_flop.cpp
@@ -0,0 +1,47 @@
+#include <cassert>
+
+#include "./circuit.h"
+#include "./connection.h"
+#include "./relay.h"
+#include "./traverser.h"
+
+int main()
+{
+    Relays::Circuit flip_flop;
+    flip_flop.ordered = true;
+
+    flip_flop._relays.resize(2);
+    Relays::Relay& r1 = flip_flop._relays.at(0);
+    Relays::Relay& r2 = flip_flop._relays.at(1);
+
+    flip_flop._connections.resize(2);
+    Relays::Connection& conn_r1_r2_cmd_inv = flip_flop._connections.at(0);
+    Relays::Connection& conn_r2_r1_cmd = flip_flop._connections.at(1);
+
+    conn_r1_r2_cmd_inv.from = 0;
+    conn_r1_r2_cmd_inv.to = 1;
+    conn_r1_r2_cmd_inv.invert = true;
+    conn_r1_r2_cmd_inv.slot = Relays::Slot::Command;
+
+    conn_r2_r1_cmd.from = 1;
+    conn_r2_r1_cmd.to = 0;
+    conn_r2_r1_cmd.invert = true;
+    conn_r2_r1_cmd.slot = Relays::Slot::Command;
+
+    Relays::Traverser traverser;
+
+    r1.input = true;
+    r2.input = true;
+
+    traverser.transfer(flip_flop);
+
+    bool desired_state = true;
+    for (int i = 0; i < 100; ++i)
+    {
+        assert(r1.output == desired_state && r2.output == desired_state);
+        desired_state = !desired_state;
+        traverser.traverse(flip_flop);
+    }
+
+    return 0;
+}
